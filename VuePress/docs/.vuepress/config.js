839ba793ec26ff017c9cc0abbfccc2ccd83087da
Jkevin <kevinjiang.366@gmail.com>
2022-10-21 08:34:43 +0800

    提交vuepress的代码

---
+++ VuePress/docs/.vuepress/config.js
@@ -0,0 +1,54 @@
+import { defineUserConfig, defaultTheme } from "vuepress";
+
+export default defineUserConfig({
+  lang: "zh-CN",
+  title: "你好， VuePress ！",
+  description: "这是我的第一个 VuePress 站点",
+  //新增导航条的配置
+  theme: defaultTheme({
+    // tab栏的图标; 图片 / 会自动去public文件夹里找图片
+    logo: "https://vuejs.org/images/logo.png", // 顶部导航条
+
+    navbar: [
+      {
+        text: "介绍",
+        link: "/pages/introduce.md",
+      }, // NavbarGroup
+      {
+        text: "教程",
+        children: [
+          {
+            text: "安装指南",
+            link: "/pages/learnJTs/install_guide.md", // 该元素将一直处于激活状态
+            activeMatch: "/pages/learnJTs/install_guide.md",
+          },
+          {
+            text: "API使用",
+            link: "/pages/learnJTs/detail_usage.md",
+            activeMatch: "/pages/learnJTs/detail_usage.md",
+          },
+          {
+            text: "待定...",
+            link: "/pages/other/other.md",
+          },
+        ],
+      },
+    ],
+    sidebar: {
+      "/guide/": [
+        {
+          text: "guide",
+          collapsible: true,
+          children: ["/guide/README.md", "/guide/getting-started.md"],
+        },
+        {
+          text: "guide",
+          collapsible: true,
+          children: ["/guide/README.md", "/guide/getting-started.md"],
+        },
+      ],
+    },
+    // github地址
+    repo: "https://github.com/JKevin-5/Font-endPractice",
+  }),
+});
